1b0880697001737a2329f0ad4255120492cb27b5
Luka Bićan <luka.bican@outlook.com>
2023-05-14 15:11:34 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StudentsComponent } from './students/students.component';
+import { StudentDetailComponent } from './students/student-detail/student-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { AuthExpiredInterceptor } from './interceptors/auth-expired.inteceptor';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StudentFormComponent } from './students/student-form/student-form.component';
+import { StudentCreateComponent } from './students/student-create/student-create.component';
+import { StudentEditComponent } from './students/student-edit/student-edit.component';
+import { ToastrModule } from 'ngx-toastr';
+import { ForbiddenPageComponent } from './forbidden-page/forbidden-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentsComponent,
+    StudentDetailComponent,
+    LoginComponent,
+    MainComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    StudentFormComponent,
+    StudentCreateComponent,
+    StudentEditComponent,
+    ForbiddenPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthExpiredInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
